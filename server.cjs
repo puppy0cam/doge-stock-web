f38e52e5ac530df589dad891cbd96703198612e4
puppy0cam <7194673+puppy0cam@users.noreply.github.com>
2020-03-30 18:19:05 +1000

    add global knex config

--- server.cjs
+++ server.cjs
@@ -27,6 +27,8 @@ const {
   getConfig,
 } = require('./credentials.cjs');
 
+/** @type {import('knex')} */
+let globalKnex;
 const playerIdRegex = /^[abcdefghijklmnopqrstuvwxyz0123456789-]+$/;
 // eslint-disable-next-line max-len
 /** @type {Map<string, {id:string;first_name:string;last_name:string;username:string;photo_url:string;auth_date:string;hash:string;}>} */
@@ -926,6 +928,7 @@ async function onReceiveRequest(request, response) {
 
     const oldConfig = currentConfig;
     currentConfig = newConfig;
+    globalKnex = knexModule(currentConfig.globalDatabaseCredentials);
     if (newConfig.httpListeningPort !== oldConfig.httpListeningPort) {
       console.log('Reloading http server');
       reloadHttpServer();
@@ -938,6 +941,7 @@ async function onReceiveRequest(request, response) {
     }
     reloadGameAmqp();
   });
+  globalKnex = knexModule(currentConfig.globalDatabaseCredentials);
 
   await Promise.all([
     reloadHttpServer(),
