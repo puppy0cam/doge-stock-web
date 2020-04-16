af9b0123597c28cafcb915395d6f9ba9a57f46ea
puppy0cam <7194673+puppy0cam@users.noreply.github.com>
2020-04-16 16:55:59 +1000

    use relative paths in route

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,17 +4,17 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') },
-      { path: 'duels', component: () => import('pages/DuelsView.vue') },
-      { path: 'offers', component: () => import('pages/OffersView.vue') },
-      { path: 'deals', component: () => import('pages/DealsView.vue') },
-      { path: 'guilds', component: () => import('pages/GuildList.vue') },
-      { path: 'players', component: () => import('pages/PlayerList.vue') },
-      { path: 'guildmembers/:server/:tag', component: () => import('pages/GuildMembers.vue'), props: true },
-      { path: 'playerhistory/:server/:id', component: () => import('pages/PlayerHistory.vue'), props: true },
-      { path: 'auth/error/outdated', component: () => import('pages/AuthOutdated.vue') },
-      { path: 'auth/error/invalid', component: () => import('pages/AuthInvalid.vue') },
-      { path: 'auth/success/:token', component: () => import('pages/AuthSuccess.vue'), props: true },
+      { path: '', component: () => import('../pages/Index.vue') },
+      { path: 'duels', component: () => import('../pages/DuelsView.vue') },
+      { path: 'offers', component: () => import('../pages/OffersView.vue') },
+      { path: 'deals', component: () => import('../pages/DealsView.vue') },
+      { path: 'guilds', component: () => import('../pages/GuildList.vue') },
+      { path: 'players', component: () => import('../pages/PlayerList.vue') },
+      { path: 'guildmembers/:server/:tag', component: () => import('../pages/GuildMembers.vue'), props: true },
+      { path: 'playerhistory/:server/:id', component: () => import('../pages/PlayerHistory.vue'), props: true },
+      { path: 'auth/error/outdated', component: () => import('../pages/AuthOutdated.vue') },
+      { path: 'auth/error/invalid', component: () => import('../pages/AuthInvalid.vue') },
+      { path: 'auth/success/:token', component: () => import('../pages/AuthSuccess.vue'), props: true },
     ],
   },
 ];
@@ -23,7 +23,7 @@ const routes = [
 if (process.env.MODE !== 'ssr') {
   routes.push({
     path: '*',
-    component: () => import('pages/Error404.vue'),
+    component: () => import('../pages/Error404.vue'),
   });
 }
 
